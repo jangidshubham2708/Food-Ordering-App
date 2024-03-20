edd45293ffb609ce54cfe52df9b7c8db83feb3a0
Shubham kumar Jangid <144809042+jangidshubham2708@users.noreply.github.com>
2024-03-20 20:51:44 +0530

    day 4
    contact us

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+  export const BACKFROUND_URL =
+  "https://variety.com/wp-content/uploads/2022/09/OnlyFans-Logo.png";
